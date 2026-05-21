dff3cfd0a8c7dbf5332278b70896e28d9e816b99
Daniel Kravets <danielkravets2005@gmail.com>
2026-05-21 16:52:15 +0200

    Avance joc.py

--- quizzbattle/services/joc.py
+++ quizzbattle/services/joc.py
@@ -1,8 +1,21 @@
 from models.usuari import Usuari
+from database.database import obtenir_nombreCuestionarios,conn
 
 def jugar_questionari(usuari: Usuari): #questionari individual
     print("\n--- JUGAR QÜESTIONARI INDIVIDUAL ---")
-    
+    noms_questionaris=obtenir_nombreCuestionarios(conn)
+    noms=[]
+    for i in noms_questionaris:
+        noms.append(i)
+
+    print("Questionaris:")
+    for i in range(len(noms)):
+        print(f"{i+1}. {noms[i]}")
+
+    num_q=int(input("Introdueix el numero de questionari: "))
+
+    questionari_seleccionat=noms[num_q-1]
+
 
 
 
